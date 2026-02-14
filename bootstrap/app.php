1e0f37193c5cfc03e06864f2c7c0a4b1aca076ef
Oussama <oussama.oubaha24@ump.ac.ma>
2026-02-15 00:35:55 +0100

    Backend fix: Explicitly bypass CSRF for API

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
         $middleware->trustProxies(at: '*');
+        $middleware->validateCsrfTokens(except: [
+            'api/*',
+            '/login',
+            '/logout'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
