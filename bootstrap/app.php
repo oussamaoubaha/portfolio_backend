e78384e3da804aa1242e9ca70d629468dea5967b
Oussama <oussama.oubaha24@ump.ac.ma>
2026-02-14 21:08:26 +0100

    Fix: Add TrustedProxies for Railway load balancer

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
